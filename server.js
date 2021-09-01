63c790d63acf3625b37f8764d9e5bdd0b70072a6
Horacinis <horacinis@gmail.com>
2021-09-01 17:14:28 +1000

    ...

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ const app = require('./app');
 
 const port = 4000;
 app.listen(process.env.PORT || port, () => {
-  console.log(`Server listening on port ${port} - In ${process.env.NODE_ENV.toUpperCase()} mode!`);
+  console.log(`Server listening on port ${process.env.PORT || port} - In ${process.env.NODE_ENV.toUpperCase()} mode!`);
 });
