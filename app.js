71b900b09d84191e31e227f1e2f1f540ea7fa15b
Horacinis <horacinis@gmail.com>
2021-09-01 17:31:04 +1000

    installed and configured cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const phonesRouter = require('./routes/products/phonesRoutes');
 const laptopsRouter = require('./routes/products/laptopsRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
