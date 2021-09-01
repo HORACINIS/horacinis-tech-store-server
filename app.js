84c10cf5e536414eb6a7f019edef76b244741b59
Horacinis <horacinis@gmail.com>
2021-09-01 16:46:53 +1000

    Morgan can now be used only in development mode and will not make deployment fail as it will not attempt to run in production mode

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const phonesRouter = require('./routes/products/phonesRoutes');
 const laptopsRouter = require('./routes/products/laptopsRoutes');
 
 const app = express();
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/products/phones', phonesRouter);
 app.use('/api/v1/products/laptops', laptopsRouter);
